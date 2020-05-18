d2a2638cbc382a0863c4bf0932d0651dc3063fbf
Alfio Martini <alfio.martini@gmail.com>
2020-05-17 22:51:21 -0300

    Added a debounce time feature before firing get search

--- static/listeners.js
+++ static/listeners.js
@@ -1,16 +1,19 @@
 addEventListener('DOMContentLoaded', listeners);
 
 // an attempt to make things as locally as possible.
-// I coild also pass the three variables below as
+// I could also pass the three variables below as
 // arguments (using currying). I have to take a look
 // at this.
 function listeners(){
     let clearR = new ClearResults();
     let input = document.querySelector('input');
     let form = document.querySelector('form');
+    let debounceTimeout = null;
 
     input.addEventListener('keypress', search_clean);
+    input.addEventListener('paste', search_clean);
     input.addEventListener('keyup', search_get);
+    input.addEventListener('change', search_get);
     form.addEventListener('submit', evalForm);
 
     function search_clean(){
@@ -18,18 +21,34 @@ function listeners(){
             clearR.clearResult();
             clearR.setClearResult(false);
         }
-        //if (clearR.getClearTable()){
-        //    clearR.hideCities();
-        //    clearR.setClearTable(false);
-        //}
+    }
+
+    function searchEvents(){
+        let input = document.querySelector('input');
+        let term = input.value;
+        //remove spaces from both sides
+        term = term.trim();
+        if (term){
+            $.ajax({
+            url: 'search/' + term,
+            method: 'get',
+            async: true, // notice this line
+            })
+            .done(function(data, status, xhr){
+                document.querySelector('#search').innerHTML = data;
+            })
+            .fail(function(xhr, status, error){
+                    console.log(error);
+            });
+        }
+        else{
+           document.querySelector('#search').innerHTML = ''; 
+        }
     }
     
-    function search_get(){
-        // data is a table of elements that is returned from the "/search" route
-        $.get('search/' + this.value, function(data){
-        document.querySelector('#search').innerHTML = data; 
-        });
-        //clearR.setClearTable(true);
+    function search_get(event){
+        clearTimeout(debounceTimeout);
+        debounceTimeout = setTimeout(searchEvents, 200);
     }
 
     function evalForm(event){
@@ -39,7 +58,7 @@ function listeners(){
         clearR.setClearTable(false);
         $.get('spinner', function(data){
              document.getElementById('spinner').innerHTML = data;
-        });
+              });
         let city = this['city'].value;
         let country = this['country'].value;
         let param = city;
