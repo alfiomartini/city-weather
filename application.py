4446418f53c332d05e5a2a6e8d4da31c2e76bd03
Alfio Martini <alfio.martini@gmail.com>
2020-11-09 19:18:52 -0300

    Updated 'about' dropdown menu

--- application.py
+++ application.py
@@ -17,7 +17,7 @@ app = Flask(__name__)
 # see https://pythonise.com/series/learning-flask/python-before-after-request
 # @app.after_request
 # def after_request(response):
-#     # Cache-Control specifies how long and in what manner should the content be cached. 
+#     # Cache-Control specifies how long and in what manner should the content be cached.
 #     # no-store specifies that the content is not to be cached by any of the caches
 #     # (public, private, server)
 #     response.headers["Cache-Control"] = "no-cache"
@@ -31,12 +31,13 @@ db = cs50.SQL('sqlite:///database/cities.db')
 
 @app.route('/')
 def index():
-    return render_template('layout.html') 
+    return render_template('layout.html')
 
-    
+
 @app.route("/search/<string:query>")
 def search(query):
     # take care of upper and lower case
+    # using sql 'like', get all cities that starts with the query strinf
     query = query.lower() + '%'
     cities = db.execute("""select id, city, country, state from cities
                              where lower(city) like  ?
@@ -44,15 +45,17 @@ def search(query):
     html = render_template("search.html", cities=cities)
     return html
 
-@app.route('/city/<int:id>', methods = ['get'])
+
+@app.route('/city/<int:id>', methods=['get'])
 def city(id):
     city = db.execute('select * from cities where id = ?', (id,))
     if not city:
         message = "Sorry, city not found in the database."
-        return render_template("failure.html", message = message)
+        return render_template("failure.html", message=message)
     lat = city[0]['lat']
     lng = city[0]['lng']
     json_7day = query_7day(lat, lng)
+    # check that json_7day is a valid jason data
     if json_7day and 'current' in json_7day:
         weather = {}
         time_zone = timezone(json_7day['timezone'])
@@ -63,39 +66,43 @@ def city(id):
         weather['feels_like'] = f"{round(current['feels_like'])}°C"
         weather['description'] = current['weather'][0]['description']
         weather['humidity'] = f"{current['humidity']} %"
-        weather['sunrise'] = datetime.fromtimestamp(current['sunrise'], tz=time_zone).strftime("%H:%M")
-        weather['sunset'] = datetime.fromtimestamp(current['sunset'], tz=time_zone).strftime("%H:%M")
+        weather['sunrise'] = datetime.fromtimestamp(
+            current['sunrise'], tz=time_zone).strftime("%H:%M")
+        weather['sunset'] = datetime.fromtimestamp(
+            current['sunset'], tz=time_zone).strftime("%H:%M")
         weather['datetime'] = datetime.now().strftime("%a, %m/%d %H:%M")
         weather['speed'] = f"{round(current['wind_speed'] * 3.6)} km/h"
 
         forecast = []
         seven_day = json_7day['daily']
-        min_temp = f"{round(seven_day[0]['temp']['min'])}°C" 
-        max_temp = f"{round(seven_day[0]['temp']['max'])}°C" 
+        min_temp = f"{round(seven_day[0]['temp']['min'])}°C"
+        max_temp = f"{round(seven_day[0]['temp']['max'])}°C"
         weather['min'] = min_temp
         weather['max'] = max_temp
-        print('min,max', min_temp, max_temp)
-        seven_day.pop(0) # remove today's weather
+        # print('min,max', min_temp, max_temp)
+        seven_day.pop(0)  # remove today's weather
         for day in seven_day:
             wdict = {}
             wdict['icon'] = day['weather'][0]['icon']
-            wdict['date'] = datetime.fromtimestamp(day['dt']).strftime("%a %m/%d")
+            wdict['date'] = datetime.fromtimestamp(
+                day['dt']).strftime("%a %m/%d")
             wdict['temp'] = str(round(day['temp']['day'])) + "°C"
             forecast.append(wdict)
         #  here we have the possibily of the server returns a complete html page
         # this route is set in href attribute of the search item link
-        # return render_template('result_page.html', weather = weather, forecast=forecast) 
+        # return render_template('result_page.html', weather = weather, forecast=forecast)
 
         # here we return a html snippet that is controlled  by a js script in 'weather.html'
-        return render_template('result.html', weather = weather, forecast=forecast)
+        return render_template('result.html', weather=weather, forecast=forecast)
     else:
         message = "Sorry, there was a problem with your request. Try again."
-        return render_template("failure.html", message = message)
+        return render_template("failure.html", message=message)
 
-@app.route('/readme', methods = ['get'])
+
+@app.route('/readme', methods=['get'])
 def readme():
     return render_template('readme.html')
 
+
 if __name__ == '__main__':
     app.run(debug=False)
-
--- weather.py
+++ weather.py
@@ -1,23 +1,27 @@
 from datetime import datetime
 import requests
 import os
-API_URL = ('http://api.openweathermap.org/data/2.5/weather?q={}&mode=json&units=metric&appid={}')
-API_7DAY = ('https://api.openweathermap.org/data/2.5/onecall?lat={}&lon={}&units=metric&appid={}')
+API_URL = (
+    'http://api.openweathermap.org/data/2.5/weather?q={}&mode=json&units=metric&appid={}')
+API_7DAY = (
+    'https://api.openweathermap.org/data/2.5/onecall?lat={}&lon={}&units=metric&appid={}')
 API_KEY = os.environ.get("CITY_WEATHER_KEY")
 
+
 def query_api(city):
-    try: 
-        print(API_URL.format(city, API_KEY))
+    try:
+        # print(API_URL.format(city, API_KEY))
         data = requests.get(API_URL.format(city, API_KEY)).json()
     except Exception as exc:
-        print(exc)
-        data = None 
+        # print(exc)
+        data = None
     return data
 
+
 def query_7day(lat, lon):
-    try: 
-        print(API_7DAY.format(lat, lon, API_KEY))
+    try:
+        # print(API_7DAY.format(lat, lon, API_KEY))
         data = requests.get(API_7DAY.format(lat, lon, API_KEY)).json()
     except:
-        data = None 
+        data = None
     return data
